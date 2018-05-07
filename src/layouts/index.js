0cc7da6984e57d6204543b45b81ebff261e3b8eb
chansuke <chansuke@georepublic.de>
2018-05-07 22:55:25 +0900

    Update style

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,45 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/header'
-import './index.css'
+import Header from '../components/Header';
+import { Conainer as BaseContainerStyles } from '../styledComponents/layout';
 
-const Layout = ({ children, data }) => (
+import './index.css';
+
+const Container = BaseContainerStyles.extend`
+  padding-top: 0;
+`;
+
+const TemplateWrapper = ({ children }) => {
   <div>
     <Helmet
-      title={data.site.siteMetadata.title}
+      title="Polling App"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
+    <Header background="background-image: linear-gradient(116deg, #08AEEA 0%, #2AF598 100%)" />
+    <Container>{children()}</Container>
+  </div>;
+};
 
-Layout.propTypes = {
-  children: PropTypes.func,
-}
-
-export default Layout
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default TemplateWrapper;
